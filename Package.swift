4f22c04bed22c529353a634ffe8c76cd7acd7c84
MD Murad Hossain <muradhossain@shadhinmusic.com>
2026-04-26 13:34:35 +0600

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ShadhinMusicSDK",
-            url: "https://github.com/shadhin-music/MyGpShadhinMusicSPM/releases/download/1.0.0/Shadhin_GP.xcframework.zip",
+            url: "https://github.com/shadhin-music/MyGpShadhinMusicSPM/raw/main/Shadhin_GP.xcframework.zip",
             checksum: "f6f3edc7f5d7caec690d914b5cf6ebac1042931439c3bbbca60f0dd09e719d43"
         )
     ]
